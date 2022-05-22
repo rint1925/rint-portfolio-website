fc41ee9431f81a555d9d40fcf8f71c7b6cffe553
rint1925 <67531356+rint1925@users.noreply.github.com>
2022-05-22 23:51:24 +0900

    updated skills.vue

--- pages/skills.vue
+++ pages/skills.vue
@@ -5,18 +5,33 @@
         
         <h2 class="mt-30">Skills</h2>
         <br>
-        <h3 class="mt-8 mb-4">アルバイトで経験したもの</h3>
-        <p>Python (Pytorch, Pandas)</p>
+        <h3 class="mt-8 mb-6">アルバイトで経験したもの</h3>
+        <p><em class="bg-pink-100">Python</em> : 時系列分析システムの開発 [Pytorch, Matplotlib, pandas他]</p>
+        <br>
+
         
-        <h3 class="mt-16 mb-4">授業・研究・自力で経験したもの</h3>
-        <p>Java / C / C# / PHP / JavaScript</p>
-        <p>Node.js / Nuxt.js / Vue.js</p>
+        <h3 class="mt-16 mb-6">授業・研究・自力で経験したもの</h3>
+        <p><em>Java</em> : 授業で描画ライブラリを用いた簡単なゲームの開発</p>
+        <br>
+        <p><em>C</em> : 授業でゲームボーイアドバンスのゲーム作成</p>
+        <br>
+        <p><em>PHP</em> : 研究でのWeb開発</p>
+        <br>
+        <p><em>JavaScript</em> : 個人・研究でのWeb開発 [Node.js, Nuxt.js, Vue.js]</p>
+        <br>
+        <p><em>HTML</em> : 個人・研究でのWeb開発</p>
+        <br>        
+        <p><em>CSS</em> : 個人・研究でのWeb開発 [Tailwind CSS]</p>
+        <br>
 
-        <h3 class="mt-16 mb-4">勉強中のもの</h3>
-        <p>Go / SQL</p>
+        <h3 class="mt-16 mb-6">勉強中のもの</h3>
+        <p><em class="bg-sky-100">GO</em> : Techbowlというオンラインコミュニティで勉強中</p>
+        <br>
+        <p><em class="bg-sky-100">SQL</em> : 授業で少し勉強, 同じくTechbowlで勉強中</p>
+        <br>
 
-        <h3 class="mt-16 mb-4">資格等</h3>
-        <p>TOEIC公開テスト 830点</p>
+        <h3 class="mt-16 mb-6">資格等</h3>
+        <p>・TOEIC公開テスト 830点</p>
 
         <NuxtLink class="inline-block link-text mt-8" to="/">[Back]</NuxtLink>
       </main>
